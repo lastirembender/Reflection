f07c18cb758c2411cb327172b178658521cb6ca6
İrem Akyol <69118634+lastirembender@users.noreply.github.com>
2022-06-03 14:38:26 +0300

    Add files via upload

---
+++ PluginSecond/LowerString.cs
@@ -0,0 +1,23 @@
+﻿using SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginSecond
+{
+ 
+    public class LowerString : IPlugin
+    {
+        public string action(string text)
+        {
+            return text.ToLower();
+        }
+
+        public string name()
+        {
+            return "Küçük Harf";
+        }
+    }
+}
